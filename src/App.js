aec76c857c5303ac38dcd4fb1bda85550907debd
prajakta bhor <prajaktabhor002@gmail.com>
2024-10-14 16:50:30 +0530

    added carcleaner home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css'
+import File from './Component/File';
+import Booking from './Component/Booking';
+import AddAnOption from './Component/AddAnOption';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+  
+    <File/>
+    {/* <Booking/> */}
+    {/* <AddAnOption/> */}
+    
+   </>
   );
 }
 
---
+++ src/Component/AddAnOption.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddAnOption = () => {
+  return (
+    <div>
+      <h1>hhiiiii</h1>
+    </div>
+  )
+}
+
+export default AddAnOption
